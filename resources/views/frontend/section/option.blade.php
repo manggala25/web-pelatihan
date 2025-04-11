aeecc32c411c6e58b49135d6bdb8860078ed8539
manggala25 <manggalaaria2505@gmail>
2025-04-11 23:39:16 +0700

    menghapus bekas template section

---
+++ resources/views/frontend/section/option.blade.php
@@ -0,0 +1,116 @@
+<!-- Section Option 1 Start -->
+<section class="solution-one py-5 my-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-xxl-6 col-xl-7 mb-xl-0 mb-3  wow animated fadeInLeft" data-wow-delay="0.1s"
+        data-wow-duration="1500ms">
+        <div class="solution-one__content sec-title me-xl-5">
+          <div class="sec-title__top-wrap">
+            <img src="{{ asset('template/frontend/assets/images/resources/top-title-logo.png') }}" alt="top-title-logo">
+            <p class="sec-title__top">Nama Section</p>
+          </div>
+          <h2 class="solution-one__heading sec-title__heading">
+            Heading Lorem ipsum dolor sit amet consectetur.
+          </h2>
+          <p class="solution-one__service-desc">
+            Paragraph Lorem ipsum dolor sit amet consectetur adipisicing elit. In tempore vel laboriosam veniam animi
+            commodi voluptatum aliquam doloremque, odio qui, at quis quam provident accusamus eum earum corrupti
+            aspernatur autem magnam nihil reprehenderit? Ipsam nam quae in quo culpa excepturi!
+          </p>
+          <!-- /.olution-one__main-tab-box -->
+        </div><!-- /.solution-one__content -->
+      </div>
+      <div class="col-xxl-6 col-xl-5 wow animated fadeInRight" data-wow-delay="0.1s" data-wow-duration="1500ms">
+        <div class="solution-one__img-col">
+          <div class="img-wrap">
+            <img
+              src="{{ $profil && $profil->img_profil ? asset('storage/' . $profil->img_profil) : asset('template/frontend/assets/images/solution/solution-1-1.jpg') }}"
+              alt="solution" class="solution-one__img img-fluid w-100">
+          </div>
+        </div><!-- /.solution-one__img-wrap -->
+      </div>
+    </div><!-- /.row -->
+  </div><!-- /.container -->
+</section>
+<!-- Section Option 1 End -->
+
+<!-- Section Option 2 Start -->
+<section class="solution-one py-5 my-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-xxl-6 col-xl-5 mb-xl-0 mb-5 wow animated fadeInRight" data-wow-delay="0.1s"
+        data-wow-duration="1500ms">
+        <div class="solution-one__img-col">
+          <div class="img-wrap">
+            <img
+              src="{{ $profil && $profil->img_profil ? asset('storage/' . $profil->img_profil) : asset('template/frontend/assets/images/solution/solution-1-1.jpg') }}"
+              alt="solution" class="solution-one__img img-fluid w-100">
+          </div>
+        </div><!-- /.solution-one__img-wrap -->
+      </div>
+      <div class="col-xxl-6 col-xl-7 wow animated fadeInLeft" data-wow-delay="0.1s" data-wow-duration="1500ms">
+        <div class="solution-one__content sec-title ms-xl-5">
+          <div class="sec-title__top-wrap">
+            <img src="{{ asset('template/frontend/assets/images/resources/top-title-logo.png') }}" alt="top-title-logo">
+            <p class="sec-title__top">Nama Section</p>
+          </div>
+          <h2 class="solution-one__heading sec-title__heading">
+            Heading Lorem ipsum dolor sit amet consectetur.
+          </h2>
+          <p class="solution-one__service-desc">
+            Paragraph Lorem ipsum dolor sit amet consectetur adipisicing elit. In tempore vel laboriosam veniam animi
+            commodi voluptatum aliquam doloremque, odio qui, at quis quam provident accusamus eum earum corrupti
+            aspernatur autem magnam nihil reprehenderit? Ipsam nam quae in quo culpa excepturi!
+          </p>
+          <!-- /.olution-one__main-tab-box -->
+        </div><!-- /.solution-one__content -->
+      </div>
+    </div><!-- /.row -->
+  </div><!-- /.container -->
+</section>
+<!-- Section Option 2 End -->
+
+<!-- Section Option 3 Start -->
+<section class="solution-one py-5 my-5">
+  <div class="container">
+    <div class="row justify-content-xl-center">
+      <div class="col-xxl-12 col-xl-10 col-12 mb-xl-0 mb-3  wow animated fadeInLeft animated text-xl-center"
+        data-wow-delay="0.1s" data-wow-duration="1500ms"
+        style="visibility: visible; animation-duration: 1500ms; animation-delay: 0.1s; animation-name: fadeInLeft;">
+        <div class="solution-one__content sec-title">
+          <div class="sec-title__top-wrap justify-content-xl-center">
+            <img src="http://127.0.0.1:8000/template/frontend/assets/images/resources/top-title-logo.png"
+              alt="top-title-logo">
+            <p class="sec-title__top">Nama Section</p>
+          </div>
+          <h2 class="solution-one__heading sec-title__heading">
+            Heading Lorem ipsum dolor sit amet consectetur.
+          </h2>
+          <p class="solution-one__service-desc">
+            Paragraph Lorem ipsum dolor sit amet consectetur adipisicing elit. In tempore vel laboriosam
+            veniam animi commodi voluptatum aliquam doloremque, odio qui, at quis quam provident accusamus
+            eum earum corrupti aspernatur autem magnam nihil reprehenderit? Ipsam nam quae in quo culpa
+            excepturi!
+          </p>
+          <!-- /.olution-one__main-tab-box -->
+        </div><!-- /.solution-one__content -->
+      </div>
+      <div class="col-xxl-12 wow animated fadeInRight animated" data-wow-delay="0.1s" data-wow-duration="1500ms"
+        style="visibility: visible; animation-duration: 1500ms; animation-delay: 0.1s; animation-name: fadeInRight;">
+        <div class="solution-one__img-col">
+          <div class="img-wrap d-flex justify-content-center">
+            {{-- <img src="http://127.0.0.1:8000/storage/profil/tCzc183l0t1YOKfYQAzyy5aQQQCukovi4vsIDwT1.png"
+              alt="solution" class="solution-one__img img-fluid w-100"
+              style="max-height: 32rem; min-height: 32rem; object-fit: cover;">
+            --}}
+            <iframe width="100%" height="500" src="https://www.youtube.com/embed/ZBiDfJflxXs?si=lENw1bJzkXAksDjU"
+              title="YouTube video player" frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          </div>
+        </div><!-- /.solution-one__img-wrap -->
+      </div>
+    </div><!-- /.row -->
+  </div><!-- /.container -->
+</section>
+<!-- Section Option 3 End -->
