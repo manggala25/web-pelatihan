50e5fac0fd38380ef689a21492522c590f7698fa
manggala25 <manggalaaria2505@gmail>
2025-04-20 00:08:13 +0700

    dashboard ringkasan

--- app/Http/Controllers/Admin/dashboardController.php
+++ app/Http/Controllers/Admin/dashboardController.php
@@ -5,12 +5,27 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Blog;
+use App\Models\Portofolio;
+use App\Models\NamaPelatihan;
+use App\Models\BentukPelatihan;
+
 class dashboardController extends Controller
 {
     public function index()
     {
+        $jumlahBlog = Blog::where('status', 'publish')->count();
+        $jumlahPortofolio = Portofolio::count();
+        $jumlahPelatihan = NamaPelatihan::count();
+        $jumlahBentukPelatihan = BentukPelatihan::count();
+
         return view('backend.dashboard.index', [
-            'showTambah' => false // atau true jika ingin ditampilkan
+            'showTambah' => false,
+            'page_title' => 'Dashboard',
+            'jumlahBlog' => $jumlahBlog,
+            'jumlahPortofolio' => $jumlahPortofolio,
+            'jumlahPelatihan' => $jumlahPelatihan,
+            'jumlahBentukPelatihan' => $jumlahBentukPelatihan,
         ]);
     }
 }
--- resources/views/backend/dashboard/index.blade.php
+++ resources/views/backend/dashboard/index.blade.php
@@ -1,13 +1,65 @@
 @extends('backend.layouts.app') <!-- Memanggil layout utama -->
 
 @section('content')
+  {{-- Tabel start --}}
   <div class="row">
-    <div class="col-12">
-      <div class="card">
-        <div class="card-body">
-          <h1>Dashboard</h1>
-        </div>
-      </div>
-    </div>
+  <div class="col-12">
+  <div class="card-header">
+  <div class="card-title">
+  <h4>Ringkasan Data</h4>
   </div>
+  </div>
+
+  <div class="card-group">
+
+    <a href="{{ route('admin.blog') }}" class="card text-decoration-none text-dark">
+    <div class="card-body">
+      <span class="btn btn-xl btn-light-info text-info btn-circle d-flex align-items-center justify-content-center">
+      <i class="ri-article-line"></i>
+      </span>
+      <h3 class="mt-3 pt-1 mb-0">{{ $jumlahBlog }}</h3>
+      <h6 class="text-muted mb-0 fw-normal">Jumlah Blog Publish</h6>
+    </div>
+    </a>
+
+    <a href="{{ route('admin.portofolio') }}" class="card text-decoration-none text-dark">
+    <div class="card-body">
+      <span
+      class="btn btn-xl btn-light-warning text-warning btn-circle d-flex align-items-center justify-content-center">
+      <i class="ri-award-line"></i>
+      </span>
+      <h3 class="mt-3 pt-1 mb-0">{{ $jumlahPortofolio }}</h3>
+      <h6 class="text-muted mb-0 fw-normal">Jumlah Portofolio</h6>
+    </div>
+    </a>
+
+    <a href="{{ route('admin.nama-pelatihan') }}" class="card text-decoration-none text-dark">
+    <div class="card-body">
+      <span class="btn btn-xl btn-light-danger text-danger btn-circle d-flex align-items-center justify-content-center">
+      <i class="ri-user-star-line"></i>
+      </span>
+      <h3 class="mt-3 pt-1 mb-0">{{ $jumlahPelatihan }}</h3>
+      <h6 class="text-muted mb-0 fw-normal">Jumlah Nama Pelatihan</h6>
+    </div>
+    </a>
+
+    <a href="{{ route('admin.bentuk-pelatihan') }}" class="card text-decoration-none text-dark">
+    <div class="card-body">
+      <span
+      class="btn btn-xl btn-light-success text-success btn-circle d-flex align-items-center justify-content-center">
+      <i class="ri-map-pin-4-line"></i>
+      </span>
+      <h3 class="mt-3 pt-1 mb-0">{{ $jumlahBentukPelatihan }}</h3>
+      <h6 class="text-muted mb-0 fw-normal">Bentuk Pelatihan</h6>
+    </div>
+    </a>
+
+  </div>
+
+
+  </div>
+  </div>
+  </div>
+  {{-- Tabel end --}}
+
 @endsection
