7ba6983df598f500341cfe387eca0ed50246974c
manggala25 <manggalaaria2505@gmail>
2025-04-20 14:38:55 +0700

    gmaps

--- resources/views/backend/informasikontak/show.blade.php
+++ resources/views/backend/informasikontak/show.blade.php
@@ -6,12 +6,12 @@
             <div class="card-body">
                 <h2 class="mb-3">{{ $informasi_kontak->nama_kontak }}</h2>
 
-                
-                
+
+
                 <div class="mb-3" hidden>
                     <strong>Icon:</strong> {!! nl2br(htmlspecialchars_decode(e($informasi_kontak->icon))) !!}
                 </div>
-                
+
                 <div class="mb-3">
                     <strong>Deskripsi:</strong> {!! nl2br(htmlspecialchars_decode(e($informasi_kontak->deskripsi))) !!}
                 </div>
@@ -20,7 +20,7 @@
                     <div class="mb-3">
                         <strong>Link Kontak:</strong>
                         <a href="{{ $informasi_kontak->link }}" target="_blank"
-                            class="btn btn-outline-primary btn-sm">{{ $informasi_kontak->link }}</a>
+                            class="btn btn-outline-primary btn-sm">{!! nl2br(htmlspecialchars_decode(e($informasi_kontak->link))) !!}</a>
                     </div>
                 @endif
 
