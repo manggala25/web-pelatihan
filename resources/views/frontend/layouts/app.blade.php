ba54af3c1a0e0cb6aa9ea961b8d1a739c3650128
manggala25 <manggalaaria2505@gmail>
2025-05-03 20:03:52 +0700

    wa

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -22,7 +22,15 @@
   {{-- <x-sidebar /> --}}
 
   <!-- WhatsApp Button -->
-  <a id="wa-button" href="https://wa.me/6281234567890" target="_blank" class="wa-btn" data-bs-toggle="tooltip"
+  <a id="wa-button" href="@if ($kontak->isNotEmpty())
+    @foreach ($kontak as $item)
+        @if ($item->nama_kontak === 'WhatsApp')
+            {{ $item->link }}
+        @endif
+    @endforeach
+  @else
+    {{ route('kontak') }}
+  @endif" target="_blank" class="wa-btn" data-bs-toggle="tooltip"
     data-bs-placement="left" title="Hubungi Kami">
     <img src="https://img.icons8.com/?size=60&id=16466&format=png&color=ffffff" alt="WhatsApp" />
   </a>
